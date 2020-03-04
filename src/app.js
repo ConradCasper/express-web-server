30195b67792beae0db7be8be9cccab000e8ec7aa
Conrad <cnrdcasper@gmail.com>
2020-03-04 15:22:29 -0500

    added public directory and index.html file. set up root path using node core module path

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+const path = require('path')
 const express = require('express')
 
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
