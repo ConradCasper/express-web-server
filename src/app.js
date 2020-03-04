dbb8fe9ed0e916aad0628bf51b2f0a957bda078a
Conrad <cnrdcasper@gmail.com>
2020-03-04 15:38:06 -0500

    created about and help.html files. went to those paths in the browser and they are working.

--- src/app.js
+++ src/app.js
@@ -9,23 +9,9 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: "Conrad",
-        age: 28
-    }, {
-        name: 'sarah',
-        age: 24
-    }])
-})
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
+
 
 app.get('/weather', (req, res) => {
     res.send({
