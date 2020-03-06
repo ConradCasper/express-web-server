6f10eee950484f6acf62091db31dd3f68eea438d
Conrad <cnrdcasper@gmail.com>
2020-03-05 23:41:14 -0500

    using handle bars to render dynamic data

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 
 
-
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Conrad Casper'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
