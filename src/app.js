8676e6a6209b59b0677c268b834d5b53a0417c15
Conrad <cnrdcasper@gmail.com>
2020-03-15 17:40:12 -0400

    returning JSON using the /weather endpoint.

--- src/app.js
+++ src/app.js
@@ -66,11 +66,24 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({
-        forecast: 'Rainy',
-        location: 'Atlanta, GA',
-        address: req.query.address
-    })
+    geocode( req.query.address, ( error, { latitude, longitude, location } ) => {
+        if (error) {
+            return res.send( { error } ) 
+        }
+
+        forecast(latitude, longitude, ( error, forecastData ) => {
+            if (error) {
+                return res.send( { error } )
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+
+            })
+        } )
+    } )
 })
 
 app.get('/help/*', (req, res) => {
